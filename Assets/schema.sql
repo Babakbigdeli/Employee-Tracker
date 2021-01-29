aee8409c052d1dde9e3c2c4108689e2197f42de5
Babakbigdeli <babakbigdeli@gmail.com>
2021-01-29 12:37:09 +0000

    Creating database schema

---
+++ Assets/schema.sql
@@ -0,0 +1,38 @@
+--Creating database
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+-- Creating DEPARTMENT Table
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+-- Creating ROLE table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+-- Creating EMPLOYEE table
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
+
+
+
