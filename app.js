8580fcd5d4f40686534857b55eac42060ef3b11b
Babakbigdeli <babakbigdeli@gmail.com>
2021-01-29 19:02:24 +0000

    First Choice function

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err
     console.log("Connected as Id" + connection.threadId)
-    firstUserInputPrompt();
+    firstInputPrompt();
 });
 
 // First User Input Prompt
 
-function firstUserInputPrompt() {
+function firstInputPrompt() {
     inquirer.prompt([
     {
     type: "list",
@@ -40,8 +40,8 @@ function firstUserInputPrompt() {
     }
     //Adding Switch cases to pick the relative function based on users choice
 ]).then(function(answer) {
-    switch (answer.action) {
-        
+    switch (answer.actions) {
+
         case "View All Employees?":
            viewAllEmployees();
         break;
@@ -75,4 +75,22 @@ function firstUserInputPrompt() {
         break;
     }
 })
+}
+// First Choice function 
+
+function viewAllEmployees() {
+    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`, 
+    function(err, res) {
+      if (err) throw err
+      console.log('\n');
+      console.log('VIEW ALL EMPLOYEES');
+      console.log('\n');
+      console.table(res)
+      firstInputPrompt();
+  })
 }
