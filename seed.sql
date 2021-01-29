b1e12441335afccfb28ac0ba2613ade4d06dbc9d
Babakbigdeli <babakbigdeli@gmail.com>
2021-01-29 13:47:47 +0000

    Seedind File Created

---
+++ seed.sql
@@ -0,0 +1,41 @@
+-- Seeding DEPARTMENT table 
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Administration and Relation");
+INSERT INTO department (name)
+VALUE ("Construction");
+INSERT INTO department (name)
+VALUE ("Production");
+
+-- Seeding ROLE table
+INSERT INTO role (title, salary, department_id)
+VALUE ("Admin Leader", 50000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Production Leader", 90000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 50000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Finance Leader", 70000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Construction Engineer", 80000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Production Engineer", 80000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Admin Clerk", 25000, 2);
+
+-- Seeding EMPLOYEE table
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Johnny", "Depp", 3, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Tom", "Hanks", 2, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Mia","Lebbof", null, 3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Chris", "Cornell", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Steve", "Probst", null, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Anita", "Karim", 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Elie", "Okobo", 2, 7);
